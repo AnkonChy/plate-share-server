71c8ccac30752281c9daba01b369d6c9cb9f8901
Ankon Chowdhury <ankonchy1@gmail.com>
2024-12-24 02:38:58 +0600

    add a food api completed

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ri84s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
+    const db = client.db("plate-share");
+    const foodCollection = db.collection("foods");
+
     app.get("/", (req, res) => {
       res.send("Hello from plateshare");
+    });
+
+    //add a food api
+    app.post("/add-food", async (req, res) => {
+      const foodData = req.body;
+      const result = await foodCollection.insertOne(foodData);
+      res.send(result);
     });
     console.log("db connected");
   } finally {
